45d1ba0d5aa84f20f1db5d347e44ed687b3dffac
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-04-28 16:16:05 +0100

    Mathematical Constant e Test

---
+++ src/ChapterFour/EConstantFactorialTest.java
@@ -0,0 +1,8 @@
+package ChapterFour;
+
+public class EConstantFactorialTest {
+    public static void main(String[] args) {
+        EConstantFactorial calculateE = new EConstantFactorial();
+        System.out.println("The value of e is " + calculateE.getEConstant(7));
+    }
+}
