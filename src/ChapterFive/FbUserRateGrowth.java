08ddf953327eb512bd340fe59e2b174f78dcce8c
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-08 04:08:00 +0100

    5.33

---
+++ src/ChapterFive/FbUserRateGrowth.java
@@ -0,0 +1,29 @@
+package ChapterFive;
+
+public class FbUserRateGrowth {
+    public static void main(String[] args) {
+        double rateOfGrowth = 0.04;
+        int initialUserBase = 1000000000;
+        double targetUserBase;
+        double secondTargetUserBase;
+        int month = 1;
+
+        for(; true; month++) {
+            targetUserBase = (initialUserBase * Math.pow(1 + rateOfGrowth, month));
+            if (targetUserBase >= 1_500_000_000) {
+                break;
+            }
+            initialUserBase = (int) targetUserBase;
+        }
+        System.out.println("It will take " + month + " months for Facebook to get to 1.5 billion users");
+
+        for(; true; month++){
+            secondTargetUserBase = initialUserBase * Math.pow(1 + rateOfGrowth, month);
+            if(secondTargetUserBase >= 2000000000){
+                break;
+            }
+            initialUserBase = (int) secondTargetUserBase;
+        }
+        System.out.println("It will take " + month + " months for Facebook to get to 2 billion users");
+    }
+}
