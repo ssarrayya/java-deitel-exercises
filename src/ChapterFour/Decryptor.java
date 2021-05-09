783c548aa96ead94dd1809a04475f1c81be3b942
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-09 13:36:29 +0100

    4.38ii

--- src/ChapterFour/Decryptor.java
+++ src/ChapterFour/Decryptor.java
@@ -7,22 +7,24 @@ public class Decryptor {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a four-digit number: ");
         int initialNumber = input.nextInt();
+
+        int decryptedFirstDigit = 0;
+        int decryptedSecondDigit = 0;
+        int decryptedThirdDigit = 0;
+        int decryptedFourthDigit = 0;
         
-        if(initialNumber >= 1000 && initialNumber < 10000) {
-            int firstDigit = initialNumber % 100 / 10;
-            firstDigit += 3;
+        int encryptedFirstDigit = initialNumber % 100 / 10;
+        int encryptedSecondDigit = initialNumber % 10;
+        int encryptedThirdDigit = initialNumber / 1000;
+        int encryptedFourthDigit = initialNumber % 1000 / 100;
 
-            int secondDigit = initialNumber % 10;
-            secondDigit += 3;
-
-            int thirdDigit = initialNumber / 1000;
-            thirdDigit += 3;
-
-            int fourthDigit = initialNumber % 1000 / 100;
-            fourthDigit += 3;
-
-            int decryptedInteger = (firstDigit * 1000 + secondDigit * 100 + thirdDigit * 10 + fourthDigit) % 10000;
-            System.out.println("The decrypted number is " + decryptedInteger);
+        for(int possibleAnswer = 0; possibleAnswer <= 9; possibleAnswer++){
+            if ((possibleAnswer + 7) % 10 == encryptedFirstDigit){ decryptedFirstDigit = possibleAnswer; }
+            if ((possibleAnswer + 7) % 10 == encryptedSecondDigit){ decryptedSecondDigit = possibleAnswer; }
+            if ((possibleAnswer + 7) % 10 == encryptedThirdDigit){ decryptedThirdDigit = possibleAnswer; }
+            if ((possibleAnswer + 7) % 10 == encryptedFourthDigit){ decryptedFourthDigit = possibleAnswer; }
         }
+            int decryptedInteger = (decryptedFirstDigit * 1000 + decryptedSecondDigit * 100 + decryptedThirdDigit * 10 + decryptedFourthDigit);
+            System.out.println("The decrypted number is " + decryptedInteger);
     }
 }
