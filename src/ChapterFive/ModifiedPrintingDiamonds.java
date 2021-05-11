2e53160001335556f509b86e2e84db2a6dd9341e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-11 13:26:05 +0100

    5.25

---
+++ src/ChapterFive/ModifiedPrintingDiamonds.java
@@ -0,0 +1,30 @@
+package ChapterFive;
+
+public class ModifiedPrintingDiamonds {
+    public void printDiamond(int userInput) {
+        if (userInput > 0 && userInput < 20 && userInput % 2 != 0) {
+            int spaces = userInput;
+            int stars = 1;
+            int lines = userInput;
+            int div = lines / 2 + 1;
+
+            for (int i = 1; i <= lines; i++) {
+                for (int s = spaces; s > 0; s--) {
+                    System.out.print(" ");
+                }
+                for (int a = stars; a > 0; a--) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+                if (i < div) {
+                    spaces -= 2;
+                    stars += 2;
+                } else if (i >= div) {
+                    spaces += 2;
+                    stars -= 2;
+                }
+            }
+        }
+
+    }
+}
