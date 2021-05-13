34808022431f662c8a8f7ef4f4a77e2f96e177cb
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-13 22:00:40 +0100

    6.31

--- src/ChapterSix/GuessTheNumberModification.java
+++ src/ChapterSix/GuessTheNumberModification.java
@@ -13,30 +13,31 @@ public class GuessTheNumberModification {
         int userGuess = scanner.nextInt();
 
         int counter = 0;
-        while(counter > 0){
-            while(userGuess != randomNumber){
-                if(userGuess >= randomNumber){
-                    System.out.println("Too high. Try again.");
-                } else if (userGuess <= randomNumber){
+
+        while(userGuess != randomNumber){
+            if(userGuess >= randomNumber){
+                System.out.println("Too high. Try again.");
+            } else if (userGuess <= randomNumber){
                 System.out.println("Too low. Try again.");
-                }
-
-                System.out.println("Guess a number between 1 and 1000");
-                userGuess = scanner.nextInt();
-                counter++;
             }
 
-            if(counter < 10 && userGuess == randomNumber){
-                System.out.println("Either you know the secret or you got lucky!");
-            } else if(counter == 10 && userGuess == randomNumber){
-                System.out.println("Aha! You know the secret!");
-            } else if (counter > 10 && userGuess == randomNumber){
-                System.out.println("You should be able to do better! Why should it take no more than 10 guesses?");
-            }
+            System.out.println("Guess a number between 1 and 1000");
+            userGuess = scanner.nextInt();
+            counter++;
         }
 
         if(userGuess == randomNumber) {
             System.out.println("Congratulations! You guessed the number");
         }
+
+        if (counter < 10 && userGuess == randomNumber) {
+            System.out.println("Either you know the secret or you got lucky!");
+        } else if (counter == 10 && userGuess == randomNumber) {
+            System.out.println("Aha! You know the secret!");
+        } else if (counter > 10 && userGuess == randomNumber) {
+            System.out.println("You should be able to do better! Why should it take no more than 10 guesses?");
+        }
+
+
     }
 }
