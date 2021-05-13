665158d9a9562adc32c51f72a582cc177da5d686
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-13 20:02:49 +0100

    6.30

---
+++ src/ChapterSix/GuessTheNumber.java
@@ -0,0 +1,30 @@
+package ChapterSix;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        SecureRandom srd = new SecureRandom();
+        int randomNumber = srd.nextInt(1000);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess a number between 1 and 1000");
+        int userGuess = scanner.nextInt();
+
+        while(userGuess != randomNumber){
+            if(userGuess >= randomNumber){
+                System.out.println("Too high. Try again.");
+            } else if (userGuess <= randomNumber){
+                System.out.println("Too low. Try again.");
+            }
+
+            System.out.println("Guess a number between 1 and 1000");
+            userGuess = scanner.nextInt();
+        }
+
+        if(userGuess == randomNumber) {
+            System.out.println("Congratulations! You guessed the number");
+        }
+    }
+}
