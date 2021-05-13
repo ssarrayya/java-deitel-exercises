07c37e01f06a3764977e9ef5beee8e0afc220ed9
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-13 21:41:07 +0100

    6.31

---
+++ src/ChapterSix/GuessTheNumberModification.java
@@ -0,0 +1,42 @@
+package ChapterSix;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class GuessTheNumberModification {
+    public static void main(String[] args) {
+        SecureRandom srd = new SecureRandom();
+        int randomNumber = srd.nextInt(1000);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess a number between 1 and 1000");
+        int userGuess = scanner.nextInt();
+
+        int counter = 0;
+        while(counter > 0){
+            while(userGuess != randomNumber){
+                if(userGuess >= randomNumber){
+                    System.out.println("Too high. Try again.");
+                } else if (userGuess <= randomNumber){
+                System.out.println("Too low. Try again.");
+                }
+
+                System.out.println("Guess a number between 1 and 1000");
+                userGuess = scanner.nextInt();
+                counter++;
+            }
+
+            if(counter < 10 && userGuess == randomNumber){
+                System.out.println("Either you know the secret or you got lucky!");
+            } else if(counter == 10 && userGuess == randomNumber){
+                System.out.println("Aha! You know the secret!");
+            } else if (counter > 10 && userGuess == randomNumber){
+                System.out.println("You should be able to do better! Why should it take no more than 10 guesses?");
+            }
+        }
+
+        if(userGuess == randomNumber) {
+            System.out.println("Congratulations! You guessed the number");
+        }
+    }
+}
