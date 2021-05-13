71eece1f0916d4c20cb75479cef2384beb29ccc2
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-13 22:37:23 +0100

    5.20

---
+++ src/ChapterFive/PiSeriesTest.java
@@ -0,0 +1,18 @@
+package ChapterFive;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PiSeriesTest {
+    PiSeries calculate;
+
+    @BeforeEach
+    void startTestsWith(){
+        calculate = new PiSeries();
+    }
+
+    @Test
+    void calculatePiSeries(){
+        calculate.calculatePiSeries();
+    }
+}
