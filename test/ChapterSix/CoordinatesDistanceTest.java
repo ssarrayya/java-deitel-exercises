f33d88e4fbbfa37d6ff94a53992ab4931379018a
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-14 10:52:33 +0100

    test

---
+++ test/ChapterSix/CoordinatesDistanceTest.java
@@ -0,0 +1,19 @@
+package ChapterSix;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CoordinatesDistanceTest {
+    CoordinatesDistance cd;
+
+    @BeforeEach
+    void startTestWith() {
+        cd = new CoordinatesDistance();
+    }
+
+    @Test
+    void distance() {
+        cd.distance(2, 5, 6, 3);
+    }
+}
