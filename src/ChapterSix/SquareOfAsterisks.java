8101b72d0a9d4c8826ecbec3ef37e21ace91d684
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-16 16:18:27 +0100

    6.18

--- src/ChapterSix/SquareOfAsterisks.java
+++ src/ChapterSix/SquareOfAsterisks.java
@@ -3,7 +3,6 @@ package ChapterSix;
 public class SquareOfAsterisks {
     public static void printSquare(int length){
         int stars = length;
-        int lines = length;
 
         for(int i = 1; i <= length; i++){
             for(int s = stars; s > 0; s--){
