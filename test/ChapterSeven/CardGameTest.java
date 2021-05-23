612503ec787ec531156cf249d89c65544c008754
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-23 12:12:52 +0100

    Card game

---
+++ test/ChapterSeven/CardGameTest.java
@@ -0,0 +1,29 @@
+package ChapterSeven;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardGameTest {
+    private CardGame cardGame;
+
+    @BeforeEach
+    void setUp() {
+        cardGame = new CardGame(2, 5);
+    }
+
+    @Test
+    void cardGameCanBeCreated() {
+        assertNotNull(cardGame);
+    }
+
+    @Test
+    void cardGameHasANumberOfPlayers() {
+        assertEquals(2, cardGame.getNumberOfPlayers());
+    }
+
+    @Test
+    void cardGameHasAFixedNumberOfCardsToDeal() {
+        assertEquals(5, cardGame.getNumberOfDealtCards());
+    }
+}
