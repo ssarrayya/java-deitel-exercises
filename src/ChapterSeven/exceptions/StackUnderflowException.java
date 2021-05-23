c0d9e71d3cc41fa22f1ca7068ff9184c65775981
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-23 11:54:08 +0100

    Card game

---
+++ src/ChapterSeven/exceptions/StackUnderflowException.java
@@ -0,0 +1,7 @@
+package ChapterSeven.exceptions;
+
+public class StackUnderflowException extends CardGameException{
+    public StackUnderflowException(String message) {
+        super(message);
+    }
+}
