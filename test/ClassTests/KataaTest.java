fe6f79d3fb07a58552c04750d5952ce543e72368
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-23 11:51:28 +0100

    array kata tests

---
+++ test/ClassTests/KataaTest.java
@@ -0,0 +1,34 @@
+package ClassTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KataaTest {
+    private final Kataa kataa = new Kataa();
+    int[] originalArray = {2, 0, 1, 5};
+    int[] secondArray = {2, 0, 1, 5};
+
+    @Test
+    void testThatArrayCanBeShuffled() {
+        Kataa.shuffle(originalArray);
+        assertFalse(Arrays.equals(originalArray, secondArray));
+    }
+
+    @Test
+    void testThatArrayCanBeSortedInAscendingOrder() {
+        Kataa.sortInAscendingOrder(originalArray);
+        assertFalse(Arrays.equals(originalArray, secondArray));
+        System.out.println(Arrays.toString(originalArray));
+    }
+
+    @Test
+    void testThatArrayCanBeSortedInDescendingOrder() {
+        Kataa.sortInDescendingOrder(originalArray);
+        assertFalse(Arrays.equals(originalArray, secondArray));
+        System.out.println(Arrays.toString(originalArray));
+    }
+}
