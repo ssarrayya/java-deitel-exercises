cadc4d23e1466e26f69d1d25e4db9e715833745d
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-27 16:53:27 +0100

    tests

--- test/ChapterSeven/normalCards/CardGameTest.java
+++ test/ChapterSeven/normalCards/CardGameTest.java
@@ -1,5 +1,6 @@
-package ChapterSeven;
+package ChapterSeven.normalCards;
 
+import ChapterSeven.normalCards.CardGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
