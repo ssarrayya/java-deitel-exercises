7ad1b84cadbd0d1cfc4337e6e0750d4e6f0c9e99
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-27 16:54:33 +0100

    tests

---
+++ test/ChapterSeven/whotCards/WhotCardTest.java
@@ -0,0 +1,60 @@
+package ChapterSeven.whotCards;
+
+import ChapterSeven.whotCards.exceptionss.*;
+import org.junit.jupiter.api.Test;
+
+import static ChapterSeven.whotCards.WhotSuits.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WhotCardTest {
+    private WhotCard card;
+
+    @Test
+    void createCircleCardsWithValues() {
+        card = WhotCard.createCircleCardsWithValues(1);
+        assertSame(CIRCLE, card.getSuit());
+    }
+
+    @Test
+    void createTriangleCardsWithValues() {
+        card = WhotCard.createTriangleCardsWithValues(3);
+        assertSame(TRIANGLE, card.getSuit());
+    }
+
+    @Test
+    void createCrossesCardsWithValues() {
+        card = WhotCard.createCrossCardsWithValues(1);
+        assertSame(CROSS, card.getSuit());
+    }
+
+    @Test
+    void createSquaresCardsWithValues() {
+        card = WhotCard.createSquareCardsWithValues(1);
+        assertSame(SQUARE, card.getSuit());
+    }
+
+    @Test
+    void createStarsCardsWithValues() {
+        card = WhotCard.createStarCardsWithValues(1);
+        assertSame(STAR, card.getSuit());
+    }
+
+//    @Test
+//    void barInvalidCircleCardValuesFromBeingCreated() {
+//        assertThrows(InvalidCircleCardValueException.class, ()-> WhotCard.createCircleCardsWithValues(6));
+//    }
+
+    @Test
+    void testThatCardsWithSameCardValuesAreTheSameWhenCompared() {
+        WhotCard firstCard = WhotCard.createCircleCardsWithValues(1);
+        WhotCard secondCard = WhotCard.createStarCardsWithValues(1);
+        assertEquals(firstCard, secondCard);
+    }
+
+    @Test
+    void testThatCardsWithSameFaceAreTheSameWhenCompared() {
+        WhotCard firstCard = WhotCard.createCircleCardsWithValues(1);
+        WhotCard secondCard = WhotCard.createCircleCardsWithValues(5);
+        assertEquals(firstCard, secondCard);
+    }
+}
