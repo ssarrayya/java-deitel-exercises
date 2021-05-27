dd0a1d037335e2246320f0db3dbf7f4377054308
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-05-27 18:40:53 +0100

    card deck population

--- src/ChapterSeven/whotCards/WhotCardDeck.java
+++ src/ChapterSeven/whotCards/WhotCardDeck.java
@@ -1,23 +1,24 @@
 package ChapterSeven.whotCards;
 
+import ChapterSeven.whotCards.WhotCardDeck;
 import ChapterSeven.whotCards.exceptionss.StackOverflowException;
 import ChapterSeven.whotCards.exceptionss.StackUnderflowException;
 
 import java.security.SecureRandom;
 
 public class WhotCardDeck {
-    private final WhotCard[] cards;
-    private int lastPushedLocation = -1;
+    private static WhotCard[] cards;
+    private static int lastPushedLocation = -1;
 
     public WhotCardDeck(int numberOfCards) {
         cards = new WhotCard[numberOfCards];
     }
 
-    public int getSize() {
+    public static int getSize() {
         return cards.length;
     }
 
-    public void push(WhotCard card) {
+    public static void push(WhotCard card) {
         if(isFull()) {
             throw new StackOverflowException("Card deck is full!");
         }
@@ -41,7 +42,7 @@ public class WhotCardDeck {
         return lastPushedLocation == -1;
     }
 
-    public boolean isFull() {
+    public static boolean isFull() {
         return lastPushedLocation == getSize() - 1;
     }
 
@@ -55,5 +56,49 @@ public class WhotCardDeck {
             cards[i] = cards[randomIndex];
             cards[randomIndex] = exchanger;
         }
+    }
+
+    public static WhotCardDeck cardDeckCanBeFilled() {
+        WhotCardDeck cardDeck = new WhotCardDeck(54);
+        for(WhotSuits whotSuits: WhotSuits.values()) {
+            switch (whotSuits) {
+                case STAR -> {
+                    for(int counter = 1; counter <= 8; counter++) {
+                        if(counter == 6) continue;
+                        push(WhotCard.createStarCardsWithValues(counter));
+                    }
+                }
+                case CIRCLE -> {
+                    for(int counter = 1; counter <= 14; counter++) {
+                        if(counter == 6 || counter == 9) continue;
+                        push(WhotCard.createCircleCardsWithValues(counter));
+                    }
+                }
+                case TRIANGLE -> {
+                    for(int counter = 1; counter <= 14; counter++) {
+                        if(counter == 6 || counter == 9) continue;
+                        push(WhotCard.createTriangleCardsWithValues(counter));
+                    }
+                }
+                case CROSS -> {
+                    for(int counter = 1; counter <= 14; counter++) {
+                        if(counter == 4 || counter == 6 || counter == 8 || counter == 9 || counter == 12) continue;
+                        push(WhotCard.createCrossCardsWithValues(counter));
+                    }
+                }
+                case SQUARE -> {
+                    for(int counter = 1; counter <= 14; counter++) {
+                        if(counter == 4 || counter == 6 || counter == 8 || counter == 9 || counter == 12) continue;
+                        push(WhotCard.createSquareCardsWithValues(counter));
+                    }
+                }
+                case WHOT -> {
+                    for(int counter = 1; counter <= 5; counter++) {
+                        push(WhotCard.createWhotCards(counter));
+                    }
+                }
+            }
+        }
+        return cardDeck;
     }
 }
