81bc7943eaa66137493eefe10340d56f6ecdb57e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-06-04 15:36:28 +0100

    4.16

---
+++ src/ChapterFour/Mystery.java
@@ -0,0 +1,16 @@
+package ChapterFour;
+
+public class Mystery {
+    public static void main(String[] args) {
+        int x = 1;
+        int total = 0;
+        while (x <= 10) {
+            int y = x * x;
+            System.out.println(y);
+            total += y;
+            ++x;
+        }
+
+        System.out.printf("Total is %d%n", total);
+    }
+}
