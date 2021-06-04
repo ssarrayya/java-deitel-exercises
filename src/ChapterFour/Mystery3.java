02830e7f3288a5381542d0fd6a60645345858f3e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-06-04 18:17:49 +0100

    4.26

---
+++ src/ChapterFour/Mystery3.java
@@ -0,0 +1,16 @@
+package ChapterFour;
+
+public class Mystery3 {
+    public static void main(String[] args) {
+        int row = 10;
+        while (row >= 1) {
+            int column = 1;
+            while (column <= 10) {
+                System.out.print(row % 2 == 1 ? "<" : ">");
+                ++column;
+            }
+            --row;
+            System.out.println();
+        }
+    }
+}
