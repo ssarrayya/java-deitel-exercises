7f3a3f0c33d76d70b2a6441e7eada145a6f86f5c
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-06-04 18:12:43 +0100

    4.24

---
+++ src/ChapterFour/ValidatingUserInput.java
@@ -0,0 +1,33 @@
+package ChapterFour;
+
+import java.util.Scanner;
+
+public class ValidatingUserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int passes = 0;
+        int failures = 0;
+        int studentCounter = 1;
+
+        while (studentCounter <= 10){
+            System.out.print("Enter result (1 = pass, 2 = fail): ");
+            int result = input.nextInt();
+            if (result == 1 || result == 2) {
+                if(result == 1) {
+                    passes = passes + 1;
+                } else {
+                    failures = failures + 1;
+                }
+            } else {
+                System.out.print("Enter result (1 = pass, 2 = fail): ");
+                result = input.nextInt();
+            }
+            studentCounter = studentCounter + 1;
+        }
+
+        System.out.printf("Passed: %d%nFailed: %d%n", passes, failures);
+        if (passes > 8)
+            System.out.println("Bonus to instructor!");
+    }
+}
