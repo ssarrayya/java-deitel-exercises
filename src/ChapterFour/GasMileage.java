5e86ebae377ff2acf8304abca662f137d2f2b7ee
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-06-11 21:58:03 +0100

    4.17

--- src/ChapterFour/GasMileage.java
+++ src/ChapterFour/GasMileage.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 public class GasMileage {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the miles driven for this trip: ");
-        int milesDriven = input.nextInt();
-
-        System.out.println("Enter the gallons used for this trip: ");
-        int gallonsUsed = input.nextInt();
-
-        int milesPerGallon = milesDriven / gallonsUsed;
-        int totalMilesPerGallon = 0;
+        int vehicleOwnerInput;
         int tripCounter = 0;
-
-        while (milesDriven > 0 && gallonsUsed > 0) {
-            totalMilesPerGallon +=  milesPerGallon;
-            tripCounter++;
-
+        int milesPerGallon = 0;
+        int totalMilesPerGallon = milesPerGallon;
+        
+        do {
             System.out.println("Enter the miles driven for this trip: ");
-            milesDriven = input.nextInt();
+            int milesDriven = input.nextInt();
 
             System.out.println("Enter the gallons used for this trip: ");
-            gallonsUsed = input.nextInt();
-        }
-        System.out.println("The miles per gallon used for a trip is "  + milesPerGallon);
+            int gallonsUsed = input.nextInt();
+
+            milesPerGallon = milesDriven / gallonsUsed;
+            System.out.println("The miles per gallon used for a trip is " + milesPerGallon);
+            totalMilesPerGallon += milesPerGallon;
+            
+            tripCounter++;
+
+            System.out.println("Enter 1 to input miles driven and gallons used or press any key to cancel");
+            vehicleOwnerInput = input.nextInt();
+        } while(vehicleOwnerInput == 1);
         System.out.println("The total miles per gallon used for the " + tripCounter + " trips is " + totalMilesPerGallon);
     }
+
 }
