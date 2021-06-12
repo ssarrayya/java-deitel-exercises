4943750e369ef7a56d016894f1720b1d7fbf1241
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-06-12 13:13:47 +0100

    7.14

---
+++ src/ChapterSeven/VariableLengthArgumentList.java
@@ -0,0 +1,23 @@
+package ChapterSeven;
+
+public class VariableLengthArgumentList {
+    //Write an application that calculates the product of a series of integers that are passed to method product using a variable-length argument list. Test your method with several calls, each with a different number of arguments.
+    public static int product(int... numbers) {
+        int product = 1;
+        for(int i: numbers) {
+            product *= i;
+        }
+        return product;
+    }
+
+    public static void main(String[] args) {
+        int firstNumber = 13;
+        int secondNumber = 15;
+        int thirdNumber = 19;
+        int fourthNumber = 10;
+
+        System.out.println(product(firstNumber, secondNumber));
+        System.out.println(product(fourthNumber, firstNumber));
+        System.out.println(product(thirdNumber, firstNumber, secondNumber));
+    }
+}
