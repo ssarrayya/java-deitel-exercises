7267a3f6580b5b6c66f443b251f66832bf349dec
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-06-24 15:17:12 +0100

    variable name change

--- src/Extraa/NoDuplicates.java
+++ src/Extraa/NoDuplicates.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 
 public class NoDuplicates {
     static int[] integerArray = {1, 1, 2, 2, 3, 3};
-    static ArrayList<Integer> newArray = new ArrayList<>();
+    static ArrayList<Integer> newList = new ArrayList<>();
 
     public static void noDuplicateValue(int[] array) {
-        newArray.add(integerArray[0]);
+        newList.add(integerArray[0]);
 
         for (int i = 1; i < integerArray.length; i++) {
             boolean isDuplicate = false;
-            for (Integer integer : newArray) {
+            for (Integer integer : newList) {
                 if (integer == (integerArray[i])) {
                     isDuplicate = true;
                     break;
                 }
             }
             if(!isDuplicate){
-                newArray.add(integerArray[i]);
+                newList.add(integerArray[i]);
             }
         }
-        System.out.println(newArray.size());
+        System.out.println(newList.size());
     }
 
     public static void displayArrayList() {
-        for (Integer integer : newArray) {
+        for (Integer integer : newList) {
             System.out.println(integer);
         }
     }
