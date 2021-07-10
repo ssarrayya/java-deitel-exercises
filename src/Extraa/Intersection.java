2ad25c2c94d41f85e6faeb31214b635430941bbf
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-07-10 20:17:27 +0100

    to find intersecting elements

--- src/Extraa/Intersection.java
+++ src/Extraa/Intersection.java
@@ -1,6 +1,12 @@
 package Extraa;
 
 public class Intersection {
+    //Have the function FindIntersection(strArr) read the array of strings stored in strArr which will
+    //contain 2 elements: the first element will represent a list of comma-separated numbers sorted in
+    //ascending order, the second element will represent a second list of comma-separated numbers (also
+    //sorted). Your goal is to return a comma-separated string containing the numbers that occur in
+    //elements of strArr in sorted order. If there is no intersection, return the string false.
+
     static String[] strArr = {"1, 3, 4, 7, 13, 17", "1, 2, 4, 13, 15, 17"};
 
     public static void FindIntersection(String[] strArr) {
