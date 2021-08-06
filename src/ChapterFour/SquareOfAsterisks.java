d843245a2e5967979580f56bd94b6cc35068d242
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-08-06 20:24:55 +0100

    4.29

---
+++ src/ChapterFour/SquareOfAsterisks.java
@@ -0,0 +1,31 @@
+package ChapterFour;
+
+import java.util.Scanner;
+
+public class SquareOfAsterisks {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int side = input.nextInt();
+        int lines = side;
+
+        int spaces = side - 2;
+
+        if(side >= 1 && side <= 20) {
+            for(int i = 0; i < lines; i++) {
+                if(i == 0 || i == lines - 1) {
+                    for (int stars = 0; stars < side; stars++) {
+                        System.out.print("*");
+                    }
+                } else {
+                    System.out.print("*");
+                    for (int s = 0; s < spaces; s++) {
+                        System.out.print(" ");
+                    }
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
