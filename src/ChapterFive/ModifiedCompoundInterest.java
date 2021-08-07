0ab7989713706734cc9a904b6b9d5c5eec21640e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-08-07 21:17:31 +0100

    5.14

---
+++ src/ChapterFive/ModifiedCompoundInterest.java
@@ -0,0 +1,20 @@
+package ChapterFive;
+
+import java.util.Scanner;
+
+public class ModifiedCompoundInterest {
+    public static void main(String[] args) {
+        double amount;
+        double principal = 1000.0;
+
+        System.out.printf("%s%s%n", "Year", "Amount on deposit");
+
+        for(double rate = 0.05; rate <= 0.1; rate++) {
+            for (int year = 1; year <= 10; ++year) {
+                amount = principal * Math.pow(1.0 + rate, year);
+
+                System.out.printf("%4d%,20.2f%n", year, amount);
+            }
+        }
+    }
+}
