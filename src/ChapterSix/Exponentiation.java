4b8ed90e3f4c25ba803255621f0a21e184fe2995
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-09-14 19:18:10 +0100

    6.14

---
+++ src/ChapterSix/Exponentiation.java
@@ -0,0 +1,17 @@
+package ChapterSix;
+
+public class Exponentiation {
+    public static void integerPower(int base, int exponent) {
+        int result = base;
+
+        for (int i = 1; i < exponent; i++) {
+            result *= base;
+        }
+
+        System.out.printf("%d", result);;
+    }
+
+    public static void main(String[] args) {
+        integerPower(2, 3);
+    }
+}
