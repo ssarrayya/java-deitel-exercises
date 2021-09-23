12c3e73e7315bec21bfeda2794d31364c18ce8e4
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-09-23 18:10:41 +0100

    with questions

--- src/ChapterTwo/CarpoolSavingsCalculator.java
+++ src/ChapterTwo/CarpoolSavingsCalculator.java
@@ -1,8 +1,22 @@
+/*
+    2.35 (Car-Pool Savings Calculator) Research several car-pooling websites. Create an application
+that calculates your daily driving cost, so that you can estimate how much money could be saved by
+carpooling, which also has other advantages such as reducing carbon emissions and reducing traffic
+congestion. The application should input the following information and display the user’s cost per
+day of driving to work:
+a) Total miles driven per day.
+b) Cost per gallon of gasoline.
+c) Average miles per gallon.
+d) Parking fees per day.
+e) Tolls per day.
+
+ */
+
 package ChapterTwo;
 
 import java.util.Scanner;
 
-public class CarPoolSavingsCalculator {
+public class CarpoolSavingsCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
--- src/ChapterTwo/WorldPopulationGrowthCalculator.java
+++ src/ChapterTwo/WorldPopulationGrowthCalculator.java
@@ -1,3 +1,8 @@
+/*
+    2.34 (World Population Growth Calculator) Use the web to determine the current world population and the annual world population growth rate. Write an application that inputs these values,
+then displays the estimated world population after one, two, three, four and five years
+ */
+
 package ChapterTwo;
 
 public class WorldPopulationGrowthCalculator {
