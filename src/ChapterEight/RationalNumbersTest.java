d67c70f711e3ffce6123a858582ebda8e46cb588
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-09-23 17:00:14 +0100

    8.15

--- src/ChapterEight/RationalNumbersTest.java
+++ src/ChapterEight/RationalNumbersTest.java
@@ -6,10 +6,12 @@ public class RationalNumbersTest {
         RationalNumbers rn1 = new RationalNumbers(1, 2);
         RationalNumbers rn2 = new RationalNumbers(1, 3);
 
-        System.out.println(RationalNumbers.addition(rn1, rn2));
-        System.out.println(RationalNumbers.subtraction(rn1, rn2));
-        System.out.println(RationalNumbers.multiplication(rn1, rn2));
-        System.out.println(RationalNumbers.division(rn1, rn2));
+        System.out.printf("%s + %s = %s%n", rn1, rn2, RationalNumbers.addition(rn1, rn2));
+        System.out.printf("%s - %s = %s%n", rn1, rn2,RationalNumbers.subtraction(rn1, rn2));
+        System.out.printf("%s * %s = %s%n", rn1, rn2,RationalNumbers.multiplication(rn1, rn2));
+        System.out.printf("%s / %s = %s%n", rn1, rn2,RationalNumbers.division(rn1, rn2));
+        System.out.println();
 
+        System.out.printf("%s + %s = %s%n", rn1, rn2, RationalNumbers.addition(rn1, rn2).toFloatFormat(4));
     }
 }
