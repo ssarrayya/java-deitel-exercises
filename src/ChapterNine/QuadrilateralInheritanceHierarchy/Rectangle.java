49cca1fc810a8e88da3d72c69bc68665db633df8
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-09-24 16:26:47 +0100

    9.8

---
+++ src/ChapterNine/QuadrilateralInheritanceHierarchy/Rectangle.java
@@ -0,0 +1,16 @@
+package ChapterNine.QuadrilateralInheritanceHierarchy;
+
+public class Rectangle extends Quadrilateral{
+    public Rectangle(Point firstPoint, Point secondPoint, Point thirdPoint, Point fourthPoint) {
+        super(firstPoint, secondPoint, thirdPoint, fourthPoint);
+    }
+
+    public double getArea(){
+        return (getSecondPoint().getXCoordinate() - getFirstPoint().getXCoordinate()) * (getThirdPoint().getXCoordinate() - getSecondPoint().getXCoordinate());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.4f", "The area of a rectangle is", getArea());
+    }
+}
---
+++ src/ChapterNine/QuadrilateralInheritanceHierarchy/Trapezoid.java
@@ -0,0 +1,30 @@
+package ChapterNine.QuadrilateralInheritanceHierarchy;
+
+public class Trapezoid extends Quadrilateral{
+    private int height;
+
+    public Trapezoid(Point firstPoint, Point secondPoint, Point thirdPoint, Point fourthPoint, int height) {
+        super(firstPoint, secondPoint, thirdPoint, fourthPoint);
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        if(height < 0.0) {
+            throw new IllegalArgumentException("Enter a height greater than 0!");
+        }
+        this.height = height;
+    }
+
+    public double getArea() {
+        return (0.5 * ((getSecondPoint().getXCoordinate()) - (getFirstPoint().getXCoordinate())) + (getFourthPoint().getXCoordinate() - getThirdPoint().getXCoordinate())) * height;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.4f", "The area of a trapezoid is", getArea());
+    }
+}
