96cf4391efdbbad7769a9583911c2874425d2fea
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-10 21:02:48 +0100

    9.15

---
+++ src/chapter9/employee/HourlyEmployee.java
@@ -0,0 +1,57 @@
+package chapter9.employee;
+
+public class HourlyEmployee extends Employee{
+    private double hours;
+    private double wages;
+
+    public HourlyEmployee(String firstName, String lastName, String socialSecurityNumber, double wages, double hours) {
+        super(firstName, lastName, socialSecurityNumber);
+        if (hours < 0.0 || hours > 168) {
+            throw new IllegalArgumentException("Hours worked must be more than 0");
+        }
+        this.hours = hours;
+        if(wages < 0) {
+            throw new IllegalArgumentException("Wages should be more than 0");
+        }
+        this.wages = wages;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        if (hours < 0.0 || hours > 168) {
+            throw new IllegalArgumentException("Hours worked must be more than 0");
+        }
+        this.hours = hours;
+    }
+
+    public double getWages() {
+        return wages;
+    }
+
+    public void setWages(double wages) {
+        if(wages < 0) {
+            throw new IllegalArgumentException("Wages should be more than 0");
+        }
+        this.wages = wages;
+    }
+
+    public double earnings() {
+        int maxHours = 40;
+        double overTime = 1.5;
+        double earnings = hours * wages;
+        if(hours > maxHours) {
+            earnings = (hours - maxHours) * overTime * wages + (maxHours * wages);
+        }
+        return earnings;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n" +
+                "Hours worked: " + hours + "\n" +
+                "Wages per hour: " + wages;
+    }
+}
