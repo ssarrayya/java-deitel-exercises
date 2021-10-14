420165ae5132c46d067f5f38dfa29413e37e73e7
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:16:13 +0100

    10.13

--- src/chapter10/shapes/ThreeDimensionalShapes.java
+++ src/chapter10/shapes/ThreeDimensionalShapes.java
@@ -1,2 +1,12 @@
-package chapter10.shapes;public class ThreeDimensionalShapes {
+package chapter10.shapes;
+
+public abstract class ThreeDimensionalShapes implements Shape{
+    abstract double getArea();
+    abstract double getVolume();
+
+    @Override
+    public String toString() {
+        return "Area of the three dimensional shape is {}" + getArea() +
+                "\n and Volume is {}" + getVolume();
+    }
 }
