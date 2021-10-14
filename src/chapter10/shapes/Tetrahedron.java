11d73355c51935fbabdc99a4e2ef01afb399fa1d
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:15:43 +0100

    10.13

--- src/chapter10/shapes/Tetrahedron.java
+++ src/chapter10/shapes/Tetrahedron.java
@@ -1,2 +1,24 @@
-package chapter10.shapes;public class Tetrahedron {
+package chapter10.shapes;
+
+public class Tetrahedron extends ThreeDimensionalShapes{
+    private double edge;
+
+    public Tetrahedron(double edge) {
+        this.edge = edge;
+    }
+
+    @Override
+    double getArea() {
+        return Math.sqrt(3) * Math.pow(edge, 2);
+    }
+
+    @Override
+    double getVolume() {
+        return Math.pow(edge, 3) / (6 * Math.sqrt(2));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.2f%n%s%.2f", "Surface area of tetrahedron: ", getArea(), "Volume of tehrahedron: ", getVolume());
+    }
 }
