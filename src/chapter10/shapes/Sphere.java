e9f67796678401a40ffdc40178d37560b503fc78
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:15:13 +0100

    10.13

--- src/chapter10/shapes/Sphere.java
+++ src/chapter10/shapes/Sphere.java
@@ -1,2 +1,24 @@
-package chapter10.shapes;public class Sphere {
+package chapter10.shapes;
+
+public class Sphere extends ThreeDimensionalShapes{
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    double getVolume() {
+        return 4 * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.2f%n%s%.2f", "Surface area of sphere: ", getArea(), "Volume of sphere: ", getVolume());
+    }
 }
