0a468fcc5d81e59bf2d254755779aa55433bf6cb
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:16:48 +0100

    10.13

--- src/chapter10/shapes/Triangle.java
+++ src/chapter10/shapes/Triangle.java
@@ -1,2 +1,21 @@
-package chapter10.shapes;public class Triangle {
+package chapter10.shapes;
+
+public class Triangle extends TwoDimensionalShapes{
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return (base * height) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.2f", "The area of the triangle is ", getArea());
+    }
 }
