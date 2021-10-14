a91a010cb1b99a5e025fed1151c52da76e60cb37
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:14:24 +0100

    10.13

--- src/chapter10/shapes/Circle.java
+++ src/chapter10/shapes/Circle.java
@@ -1,2 +1,19 @@
-package chapter10.shapes;public class Circle {
+package chapter10.shapes;
+
+public class Circle extends TwoDimensionalShapes{
+    private double diameter;
+
+    public Circle(double diameter) {
+        this.diameter = diameter;
+    }
+
+    @Override
+    public double getArea() {
+        return (Math.PI * diameter * diameter) / 4;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.2f", "The area of the circle is ", getArea());
+    }
 }
