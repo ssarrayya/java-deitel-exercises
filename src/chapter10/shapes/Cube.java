90abafd8a2a36f115a925ce525332a801eebc421
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:14:51 +0100

    10.13

--- src/chapter10/shapes/Cube.java
+++ src/chapter10/shapes/Cube.java
@@ -1,2 +1,24 @@
-package chapter10.shapes;public class Cube {
+package chapter10.shapes;
+
+public class Cube extends ThreeDimensionalShapes{
+    private double edge;
+
+    public Cube(double edge) {
+        this.edge = edge;
+    }
+
+    @Override
+    public double getArea() {
+        return 6 * Math.pow(edge, 2);
+    }
+
+    @Override
+    double getVolume() {
+        return Math.pow(edge, 3);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.2f%n%s%.2f", "Surface area of cube: ", getArea(), "Volume of cube: ", getVolume());
+    }
 }
