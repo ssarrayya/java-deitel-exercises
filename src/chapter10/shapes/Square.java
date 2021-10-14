a4e97d23b0f3fa4ee121265b4199b0983ca10c05
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:15:26 +0100

    10.13

--- src/chapter10/shapes/Square.java
+++ src/chapter10/shapes/Square.java
@@ -1,2 +1,19 @@
-package chapter10.shapes;public class Square {
+package chapter10.shapes;
+
+public class Square extends TwoDimensionalShapes {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.2f", "The area of the square is ", getArea());
+    }
 }
