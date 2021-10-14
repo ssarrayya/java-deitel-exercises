68083c3acbf0e79169fb259a5e87f4ab3a041d1c
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:16:31 +0100

    10.13

--- src/chapter10/shapes/TwoDimensionalShapes.java
+++ src/chapter10/shapes/TwoDimensionalShapes.java
@@ -1,2 +1,10 @@
-package chapter10.shapes;public class TwoDimensionalShapes {
+package chapter10.shapes;
+
+public abstract class TwoDimensionalShapes implements Shape{
+    abstract double getArea();
+
+    @Override
+    public String toString() {
+        return "Area of the two dimensional shape is {}" + getArea();
+    }
 }
