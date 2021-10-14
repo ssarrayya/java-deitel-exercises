d897312824e12e9c1d168cb599296a9b3c8fc622
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-14 10:57:34 +0100

    added questions

--- src/chapter10/payrollsystem/Employee.java
+++ src/chapter10/payrollsystem/Employee.java
@@ -2,6 +2,15 @@ package chapter10.payrollsystem;
 
 import chapter8.Date;
 
+/*
+10.12 (Payroll System Modification) Modify the payroll system of Figs. 10.4–10.9 to include private instance
+variable birthDate in class Employee. Use class Date of Fig. 8.7 to represent an employee’s birthday. Add get
+methods to class Date. Assume that payroll is processed once per month. Create an array of Employee variables
+to store references to the various employee objects. In a loop, calculate the payroll for each Employee
+(polymorphically), and add a $100.00 bonus to the person’s payroll amount if the current month is the one
+in which the Employee’s birthday occurs.
+ */
+
 public abstract class Employee {
     private final String firstName;
     private final String lastName;
--- src/chapter10/payrollsystem/PieceWorker.java
+++ src/chapter10/payrollsystem/PieceWorker.java
@@ -2,6 +2,17 @@ package chapter10.payrollsystem;
 
 import chapter8.Date;
 
+/*
+10.14 (Payroll System Modification) Modify the payroll system of Figs. 10.4–10.9 to include an additional
+Employee subclass PieceWorker that represents an employee whose pay is based on the number of pieces of
+merchandise produced. Class PieceWorker should contain private instance variables wage (to store the
+employee’s wage per piece) and pieces (to store the number of pieces produced). Provide a concrete
+implementation of method earnings in class PieceWorker that calculates the employee’s earnings by multiplying
+the number of pieces produced by the wage per piece. Create an array of Employee variables to store references
+to objects of each concrete class in the new Employee hierarchy. For each Employee, display its String
+representation and earnings.
+ */
+
 public class PieceWorker extends Employee {
     private double wage;
     private int pieces;
