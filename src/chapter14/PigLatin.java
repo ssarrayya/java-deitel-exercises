43ebcff54c7f09cbe9da6b866a2543ddf7a5eb32
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-16 17:06:06 +0100

    14.7

--- src/chapter14/PigLatin.java
+++ src/chapter14/PigLatin.java
@@ -1,7 +1,42 @@
 package chapter14;
 
-public class PigLatin {
-    public static void main(String[] args) {
+/*
+14.7 (Pig Latin) Write an application that encodes English-language phrases into pig Latin. Pig Latin is a
+form of coded language. There are many different ways to form pig Latin phrases. For simplicity, use the
+following algorithm: To form a pig Latin phrase from an English-language phrase, tokenize the phrase into
+words with String method split. To translate each English word into a pig Latin word, place the first letter
+of the English word at the end of the word and add the letters “ay.” Thus, the word “jump” becomes “umpjay,”
+the word “the” becomes “hetay,” and the word “computer” becomes “omputercay.” Blanks between words remain as
+blanks. Assume the following: The English phrase consists of words separated by blanks, there are no
+punctuation marks and all words have two or more letters. Method printLatinWord should display each word.
+Each token is passed to method printLatinWord to print the pig Latin word. Enable the user to input the
+sentence. Keep a running display of all the converted sentences in a text area.
+*/
 
+import java.util.Scanner;
+
+public class PigLatin {
+    public static String[] splitWord(String sentence) {
+        return sentence.split(" ");
+    }
+
+    public static StringBuilder printLatinWord(String word) {
+        char[] wordArray = word.toCharArray();
+        StringBuilder latinWord = new StringBuilder();
+        for(int i = 1; i < wordArray.length; i++) {
+            latinWord.append(wordArray[i]);
+        }
+        latinWord.append(wordArray[0]).append("ay");
+        return latinWord;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence you want to translate:");
+        String sentence = scanner.nextLine();
+        String[] tokens = splitWord(sentence);
+        for(String word : tokens) {
+            System.out.print(printLatinWord(word) + " ");
+        }
     }
 }
