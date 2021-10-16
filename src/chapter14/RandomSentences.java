5a01697e0acc1c158ad0ecf3c3517927f5a11c4f
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-16 16:13:00 +0100

    with questions

--- src/chapter14/RandomSentences.java
+++ src/chapter14/RandomSentences.java
@@ -1,5 +1,17 @@
 package chapter14;
 
+/*
+14.5 (Random Sentences) Write an application that uses random-number generation to create sentences. Use
+four arrays of strings called article, noun, verb and preposition. Create a sentence by selecting a word at
+random from each array in the following order: article, noun, verb, preposition, article and noun. As each
+word is picked, concatenate it to the previous words in the sentence. The words should be separated by
+spaces. When the final sentence is output, it should start with a capital letter and end with a period. The
+application should generate and display 20 sentences. The article array should contain the articles "the",
+"a", "one", "some" and "any"; the noun array should contain the nouns "boy", "girl", "dog", "town" and "car";
+the verb array should contain the verbs "drove", "jumped", "ran", "walked" and "skipped"; the preposition
+array should contain the prepositions "to", "from", "over", "under" and "on"
+ */
+
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
