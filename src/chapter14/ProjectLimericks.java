b2cecde404be614231817f327c66468d7ae47a13
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-16 16:15:51 +0100

    with questions

--- src/chapter14/ProjectLimericks.java
+++ src/chapter14/ProjectLimericks.java
@@ -1,5 +1,12 @@
 package chapter14;
 
+/*
+14.6 (Project: Limericks) A limerick is a humorous five-line verse in which the first and second lines rhyme
+with the fifth, and the third line rhymes with the fourth. Using techniques similar to those developed in
+Exercise 14.5, write a Java application that produces random limericks. Polishing this application to produce
+good limericks is a challenging problem, but the result will be worth the effort
+ */
+
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
@@ -7,7 +14,7 @@ import java.security.SecureRandom;
 public class ProjectLimericks {
     @Test
     void obtainingRandomLimerick(){
-        String[] threeRhymer = {"There was a young lady of station\n", "I love man was her sole exclamation\n", "Isle of Man is the true explanation"};
+        String[] threeRhymer = {"There was a young lady of station\n", "I love man was her sole exclamation\n", "Isle of Man is the true explanation\n"};
         String[] twoRhymer = {"But when men cried, \"You flatter\"\n", "She replied, \"Oh! no matter!\n"};
 
         SecureRandom rdm = new SecureRandom();
