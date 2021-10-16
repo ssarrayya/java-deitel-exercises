e2b26ed12414bd51e0958f9e196ca6fd40ff6b79
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-16 16:10:14 +0100

    with questions

--- src/chapter14/ComparingPortionsOfStrings.java
+++ src/chapter14/ComparingPortionsOfStrings.java
@@ -1,5 +1,12 @@
 package chapter14;
 
+/*
+14.4 (Comparing Portions of Strings) Write an application that uses String method regionMatches to compare
+two strings input by the user. The application should input the number of characters to be compared and the
+starting index of the comparison. The application should state whether the strings are equal. Ignore the
+case of the characters when performing the comparison.
+ */
+
 import org.junit.jupiter.api.Test;
 
 public class ComparingPortionsOfStrings {
