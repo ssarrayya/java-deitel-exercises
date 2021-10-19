2c2b8f79e5c45cf94b6ec9ac4d5254e93c0bc94e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-19 16:04:17 +0100

    14.10

--- src/chapter14/StringCases.java
+++ src/chapter14/StringCases.java
@@ -1,2 +1,14 @@
-package chapter14;public class StringCases {
+package chapter14;
+
+/*
+14.10 (Displaying Strings in Uppercase and Lowercase) Write an application that inputs a line of
+text and outputs the text twice—once in all uppercase letters and once in all lowercase letters.
+ */
+
+public class StringCases {
+    public static void main(String[] args) {
+        String text = ReverseSentence.collectSentence();
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+    }
 }
